6283c7452f6f44456c8f6eca4eb68bfe041b57b2
Devin Wood <devin.wood@turingmedical.com>
2024-12-21 17:43:01 -0600

    Toggle diagnostics for lsp

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.keymap.set('n', '<leader>td', function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { silent = true, noremap = true })
+
 vim.api.nvim_set_option("clipboard", "unnamed")
 
 
